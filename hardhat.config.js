d813cb5b4afa9da4e6eca30c40c3c64c329beec3
Jenna Chestnut <jennabot5000@gmail.com>
2021-11-26 12:49:32 -0600

    switched to rinkeby testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
-const {alchemyApiUrl, ropstenPrivateKey} = require('./secret.json');
+const {alchemyApiUrl, rinkebyPrivateKey} = require('./secret.json');
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -20,9 +20,9 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 module.exports = {
   solidity: "0.8.0",
   networks: {
-    ropsten: {
+    rinkeby: {
       url: alchemyApiUrl,
-      accounts: [ropstenPrivateKey]
+      accounts: [rinkebyPrivateKey]
     }
   }
 };
