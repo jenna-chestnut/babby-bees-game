41a5c4515dcd22171b54d4ed6e9378be2649dfd1
Jenna Chestnut <jennabot5000@gmail.com>
2021-11-25 20:55:08 -0600

    minting 3 NFTs in deploy file

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -21,11 +21,17 @@ const main = async () => {
 
   let txn;
 
+  txn = await gameContract.mintCharacterNFT(0);
+  await txn.wait();
+  console.log("Minted NFT #1");
+
+  txn = await gameContract.mintCharacterNFT(1);
+  await txn.wait();
+  console.log("Minted NFT #1");
+
   txn = await gameContract.mintCharacterNFT(2);
   await txn.wait();
-
-  let returnedTokenUri = await gameContract.tokenURI(1);
-  console.log("Token URI: ", returnedTokenUri);
+  console.log("Minted NFT #1");
 
 }
 
